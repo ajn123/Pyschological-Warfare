76d0a2745a62f06580c244c5990f6a811e48c421
ajn123 <ajn123@vt.edu>
2017-07-17 18:03:01 -0500

    added email

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,21 @@ module PsychoWarfareWebsite
 
     config.active_job.queue_adapter = :sidekiq
 
+    #These settings are for the sending out email for active admin and consequently the   devise mailer
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = 
+{
+
+  :address            => 'email-smtp.us-west-2.amazonaws.com',
+  :port               => 587,
+  :authentication     => :login,
+  :user_name          => 'AKIAJXXJVIZWJDUSFPVA',
+  :password           => 'AqnEpI+owvHZOBYXFLZ4R+jy0IYgprdBPOCYhVHs3lhY',
+  :enable_starttls_auto => true
+}
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
