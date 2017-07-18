7e4edf1b57e3ed51a31c1f06f313976d072c2bf2
ajn123 <ajn123@vt.edu>
2017-07-18 06:13:52 -0500

    added name to mail chimp api

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -2,21 +2,23 @@ class HardWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'critical'
 
-  def perform(name)
-  	UserMailer.welcome_email(name).deliver_later
-    subscribe(name)
+  def perform(user)
+  	UserMailer.welcome_email(user.email).deliver_later
+    subscribe(user)
     
   end
 
 
   # Does not work in production
-  def subscribe(email)
+  def subscribe(user)
     mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
     list_id = Rails.application.secrets.mailchimp_list_id
     result = mailchimp.lists(list_id).members.create(
       body: {
-        email_address: email,
-        status: 'subscribed'
+        email_address: user.email,
+        status: 'subscribed',
+        merge_fields: { FNAME: user.name, LNAME: "" }
     })
   end
 end
+
