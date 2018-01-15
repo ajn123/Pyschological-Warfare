2741565fffcceacecdb9cb8cd6dab2ec11c5189c
ajn123 <ajn123@vt.edu>
2018-01-14 21:47:25 -0600

    added path and image for meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
-	  include TheComments::ViewToken
+	include TheComments::ViewToken
 
-	  
   protect_from_forgery with: :exception
+
+	before_action :sharing_meta_tags
+
+
+	def sharing_meta_tags
+	  @path = request.original_url
+		@meta_image = nil
+	end
 end
