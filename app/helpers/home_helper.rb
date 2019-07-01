5a73abaea294f01c777d980e2516089afec18b2a
ajn123 <ajn123@vt.edu>
2019-07-01 00:20:14 -0500

    refacted helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+
+
+	def read_more?(article,read_more)
+		 if read_more == false 
+         article.content.html_safe 
+     else 
+         truncate(ActionView::Base.full_sanitizer.sanitize(article.content.html_safe), length: 300) 
+     end 
+	end
 end
